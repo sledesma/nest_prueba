ffbb245703865c42d04216cd94adc9ef5af43b53
sebas <sebas@Scorpion.sebas.com>
2021-03-03 22:03:48 -0300

    Correcciones para el deploy

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, () =>
-    console.log('Servidor ejecutandose en http://localhost:3000'),
-  );
+  await app.listen(80);
 }
 bootstrap();
